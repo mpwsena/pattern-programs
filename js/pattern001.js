93332281fba6e69703af2c410ce8bc5c81d242f7
Matheus Sena <mpwsena@gmail.com>
2025-03-03 22:39:25 -0300

    Create pattern001.js

---
+++ js/pattern001.js
@@ -0,0 +1,18 @@
+// [001] PATTERN PROGRAMS
+// ----------------------
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// ----------------------
+
+let pattern = "";
+
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= 5; j++) {
+        pattern += "* ";
+    }
+    pattern += "\n";
+}
+console.log(pattern);
